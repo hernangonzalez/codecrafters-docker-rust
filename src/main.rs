288c314ce94d8a217a4767c5def0dbd7d85354e1
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-03-31 16:50:14 +0100

    Drop extra prints

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ use std::io::{self, Write};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Spining up mydocker ...");
-
     let args: Vec<_> = std::env::args().collect();
     if args.len() < 4 {
         return Err(anyhow::anyhow!("Not enough arguments"));
@@ -17,7 +14,6 @@ fn main() -> Result<()> {
         .args(command_args)
         .output()?;
 
-    println!("{}", output.status);
     io::stdout().write_all(&output.stdout)?;
     io::stderr().write_all(&output.stderr)?;
 
