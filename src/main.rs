953cb2140284e29b0dea607de56fe2fb67d3f10f
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-04-01 18:58:43 +0100

    Create a new root and change to it.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use std::fs;
 use std::io::{self, Write};
+use std::os::unix;
+use std::path::Path;
+use tempfile::{tempdir, TempDir};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -8,8 +12,14 @@ fn main() -> Result<()> {
         return Err(anyhow::anyhow!("Not enough arguments"));
     }
 
-    let command = &args[3];
+    let _action = &args[1];
+    let _image = &args[2];
+    let command = Path::new(&args[3]);
     let command_args = &args[4..];
+
+    let root = prepare_root(command)?;
+    unix::fs::chroot(root).context("could not change root")?;
+
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()?;
@@ -22,4 +32,18 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn prepare_root(cmd: &Path) -> Result<TempDir> {
+    let filename = cmd.file_name().context("cmd filename")?;
+    let temp_dir = tempdir()?;
+
+    let mut path = temp_dir.path().join(cmd.strip_prefix("/")?);
+    path.pop();
+    fs::create_dir(path.as_path())?;
+
+    path.push(filename);
+    fs::copy(cmd, path)?;
+
+    Ok(temp_dir)
 }
